9621bb1f0534ccfffaa0904c4f0ce3689ca817b0
SabihMajid <153662708+SabihMajid@users.noreply.github.com>
2024-01-02 22:08:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const SystemGenreteNumber = Math.floor(Math.random() * 10);
+const { UserGuess } = await inquirer.prompt({
+    type: "number",
+    name: "UserGuess",
+    message: ("guess the number between 1to10")
+});
+if (UserGuess === SystemGenreteNumber) {
+    console.log("yuo are winner");
+}
+else {
+    console.log(`better luck next time the correct number is ${SystemGenreteNumber}`);
+}
